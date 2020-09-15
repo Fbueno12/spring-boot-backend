4ef5381cd4907c2c3e0459770a934c8639f1ee0f
fbueno12 <felipebueno181@gmail.com>
2020-09-15 17:16:58 -0300

    criando validação de email existente no banco de dados

--- src/main/java/com/felipebueno/springshop/repositories/ClientRepository.java
+++ src/main/java/com/felipebueno/springshop/repositories/ClientRepository.java
@@ -2,10 +2,14 @@ package com.felipebueno.springshop.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.felipebueno.springshop.domain.Client;
 
 @Repository
-public interface ClientRepository extends JpaRepository<Client, Integer>{
+public interface ClientRepository extends JpaRepository<Client, Integer> {
+
+	@Transactional(readOnly = true)
+	Client findByEmail(String email);
 
 }
--- src/main/java/com/felipebueno/springshop/services/validation/ClientInsertValidator.java
+++ src/main/java/com/felipebueno/springshop/services/validation/ClientInsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.felipebueno.springshop.domain.Client;
 import com.felipebueno.springshop.domain.enums.ClientType;
 import com.felipebueno.springshop.payloads.NewClientPayload;
+import com.felipebueno.springshop.repositories.ClientRepository;
 import com.felipebueno.springshop.resources.exception.FieldMessage;
 import com.felipebueno.springshop.services.validation.utils.BR;
 
 public class ClientInsertValidator implements ConstraintValidator<ClientInsert, NewClientPayload> {
 
+	@Autowired
+	private ClientRepository repository;
+	
 	@Override
 	public void initialize(ClientInsert ann) {
 	}
@@ -27,6 +34,12 @@ public class ClientInsertValidator implements ConstraintValidator<ClientInsert, 
 		
 		if(payload.getClientType().equals(ClientType.PESSOAJURIDICA.getCode()) && !BR.isValidCNPJ(payload.getDocument())) {
 			list.add(new FieldMessage("document", "CNPJ inválido"));
+		}
+		
+		Client aux = repository.findByEmail(payload.getEmail());
+		
+		if(aux != null) {
+			list.add(new FieldMessage("email", "email já está cadastrado em nossa base de dados!"));
 		}
 		
 		for (FieldMessage e : list) {
